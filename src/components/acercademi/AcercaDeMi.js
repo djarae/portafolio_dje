a2db7310030d6137a4067047dd13c7838ca115d5
djarae <djaraestivill@gmail.com>
2024-12-03 13:06:52 -0300

    Portafolio simple
    Portafolio simple

--- src/components/acercademi/AcercaDeMi.js
+++ src/components/acercademi/AcercaDeMi.js
@@ -5,7 +5,7 @@ import './acercademi.css';
 function AcercaDeMi() {
   return (
         <div  class="panel panel-default"    className="divAcercaDeMi">
-            <h1 className='h1TitutloAcercaDeMi'>
+            <h1 className='h1TitutloAcercaDeMi' >
                 Acerca De Mi
             </h1>
             <h1  className='h1ParrafoAcercaDeMi'>
