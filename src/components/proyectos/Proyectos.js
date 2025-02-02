3438b5df975b1d2f30381e8f5dac6e702f432760
djarae <51816647+djarae@users.noreply.github.com>
2025-02-02 04:14:15 -0300

    Fix error celu horizontal

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,19 +1,22 @@
 import Portfolio from './portfolio/Portfolio.js';
 import PortfolioCelu from './portfoliocelu/PortfolioCelu.js';
+
 import Gastos from './gastos/Gastos.js';
 import GastosCelu from './gastoscelu/GastosCelu.js';
+
 
 import '../../styles/Proyectos.css';
 
 function Proyectos() {
   return (
   <div className='divProyectos'>
+    {/* Se crean componentes para celular horizontal  y vertical */}
     <p class="justify-content-center" className="titulo-proyecto">Proyectos</p>
-    <div className="divProyectosI"><div class="d-none d-md-flex  d-lg-flex d-xl-flex  justify-content-center" ><Portfolio></Portfolio></div></div>
-    <div className="divProyectosI"><div class="d-none d-md-flex  d-lg-flex d-xl-flex d-flex justify-content-center"  ><Gastos></Gastos></div></div>
+    <div className="divProyectosI"><div class="d-none  d-sm-flex d-md-flex  d-lg-flex d-xl-flex  justify-content-center" ><Portfolio></Portfolio></div></div>
+    <div className="divProyectosI"><div class="d-none d-sm-flex d-md-flex  d-lg-flex d-xl-flex d-flex justify-content-center"  ><Gastos></Gastos></div></div>
     <div className="divProyectosI"><div class="d-sm-none d-flex justify-content-center" ><PortfolioCelu></PortfolioCelu></div></div>
     <div className="divProyectosI"><div class="d-sm-none d-flex justify-content-center" ><GastosCelu></GastosCelu></div></div>
-
+ 
   </div>
   );
 }
