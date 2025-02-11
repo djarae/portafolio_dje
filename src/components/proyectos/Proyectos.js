cac1b61ae5356c90a2a88eec70594f2658d0d3a5
djarae <51816647+djarae@users.noreply.github.com>
2025-02-11 02:28:34 -0300

    feat Se agrega nuevo proyecto
    El proyecto es muy escalable y facil de editar, al final lo mas complejo es crear una buena "BASE" pero al crearla , ya es tirar para arriba y editar muy facilmente!!!!!

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,18 +1,23 @@
 import Proyecto from './proyecto/Proyecto.js';
 import '../../styles/Proyectos.css';
+import ProyectosTxt from "../../data/proyectos.json";
 
 function Proyectos() {
+
+  function getProyectos(){
+    let proyectos = [];
+    for (let i=0;i<ProyectosTxt.length;i++){
+       proyectos.push( <div className="divProyectosI"><div class="d-none  d-sm-flex d-md-flex  d-lg-flex d-xl-flex  justify-content-center" ><div class="container  col-6  rounded bg-dark row"><Proyecto id={i}></Proyecto></div></div></div>)
+       proyectos.push(    <div className="divProyectosI"><div class="d-sm-none d-flex  justify-content-center" ><div class="container  col-10  rounded bg-dark row"><Proyecto id={i}></Proyecto></div></div></div>)
+    }
+    return(proyectos);
+
+  }
+
   return (
   <div className='divProyectos'>
-    
     <p class="justify-content-center" className="titulo-proyecto">Proyectos</p>
-    {/*Pantallas para pc*/}
-    <div className="divProyectosI"><div class="d-none  d-sm-flex d-md-flex  d-lg-flex d-xl-flex  justify-content-center" ><div class="container  col-5  rounded bg-dark row"><Proyecto id="0"></Proyecto></div></div></div>
-    <div className="divProyectosI"><div class="d-none  d-sm-flex d-md-flex  d-lg-flex d-xl-flex  justify-content-center" ><div class="container  col-5  rounded bg-dark row"><Proyecto id="1"></Proyecto></div></div></div>
-    {/*Pantallas para celu*/}
-    <div className="divProyectosI"><div class="d-sm-none d-flex  justify-content-center" ><div class="container  col-9  rounded bg-dark row"><Proyecto id="0"></Proyecto></div></div></div>
-    <div className="divProyectosI"><div class="d-sm-none d-flex  justify-content-center" ><div class="container  col-9  rounded bg-dark row"><Proyecto id="1"></Proyecto></div></div></div>
- 
+       { getProyectos()}
   </div>
   );
 }
--- src/documentation/LINKS.md
+++ src/documentation/LINKS.md
@@ -2,3 +2,14 @@
 FUENTES CSS https://www.w3schools.com/css/css_font.asp 
 
 Recordatorio hooks de efecto y componentdidmount(& similares):https://legacy.reactjs.org/docs/hooks-effect.html
+
+# Media querys
+ https://stackoverflow.com/questions/13550541/media-min-width-max-width
+
+# Custom Hook media query en react:
+https://youtu.be/gjISBiOQf8Y?si=9VJn2itcOCA_Lxev
+
+https://dev.to/soumyarian/usescreensize-a-custom-react-hook-for-dynamic-screen-size-detection-5e59#:~:text=The%20%22useScreenSize%22%20hook%20allows%20you,frameworks%20such%20as%20Tailwind%20CSS.
+
+# Angular obtener with y height de forma dinamica para modificar:
+https://www.javatpoint.com/get-width-and-height-of-screen-using-angular
